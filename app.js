19a5cef2caa52eee441d5e25fb053391a4dc8f5a
alexizgarcia <127428560+alexizgarcia@users.noreply.github.com>
2023-03-10 11:07:10 -0600

    Create app.js
    Revision de codígo

---
+++ app.js
@@ -0,0 +1,6 @@
+#este archivo es de prueba 
+
+#....
+#...
+aqui pueden venir los comentarios de otro programados 
+ya sea corrigiendo los erroes y de una ves me puede envioar el codigo ya orregido 
